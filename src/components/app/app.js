8baf1ef1eaa4d51dec250dd976e70c2ffd1b4167
Semen <senia-2010@mail.ru>
2021-06-09 15:16:14 +0300

    finished project

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,7 +19,9 @@ export default class App extends Component {
       this.createTodoItem('Drink Coffee'),
       this.createTodoItem('Make Awesome APP'),
       this.createTodoItem('Have a lunch'),
-    ]
+    ],
+    term: '',
+    filter: 'all'
   };
 
   createTodoItem(label) {
@@ -75,7 +77,7 @@ export default class App extends Component {
 
   onToggleDone = (id) => {
     this.setState(({ todoData }) => {
-     
+
       return {
         todoData: this.toggleProperty(todoData, id, 'done')
       };
@@ -86,7 +88,7 @@ export default class App extends Component {
 
   onToggleImportant = (id) => {
     this.setState(({ todoData }) => {
-     
+
       return {
         todoData: this.toggleProperty(todoData, id, 'important')
       };
@@ -94,10 +96,47 @@ export default class App extends Component {
   };
 
 
+  onSearchChange = (term) => {
+    this.setState({ term });
+  };
 
+  onFilterChange = (filter) => {
+    this.setState({ filter });
+  };
+
+
+  search(items, term) {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.label
+        .toLowerCase()
+        .indexOf(term.toLowerCase()) > -1;
+    });
+  };
+
+
+  filter(items, filter) {
+
+    switch(filter) {
+      case 'all':
+        return items;
+        case 'active':
+        return items.filter((item) => !item.done);
+        case 'done':
+          return items.filter((item) => item.done);
+          default:
+            return items;
+    }
+  }
 
   render() {
-    const { todoData } = this.state;
+    const { todoData, term, filter } = this.state;
+
+    const visibleItems = this.filter(
+      this.search(todoData, term), filter);
     const doneCount = todoData.filter((el) => el.done).length;
 
     const todoCount = todoData.length - doneCount;
@@ -108,11 +147,14 @@ export default class App extends Component {
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
+          <SearchPanel
+            onSearchChange={this.onSearchChange} />
+          <ItemStatusFilter
+           filter={filter}
+           onFilterChange={this.onFilterChange} />
         </div>
 
-        <TodoList todos={todoData}
+        <TodoList todos={visibleItems}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
